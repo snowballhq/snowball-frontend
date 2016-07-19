6585b0eb8969c597d3e3f235e94d5370ecd78e64
James Martinez <jamescmartinez@gmail.com>
2016-07-18 17:16:33 -0700

    Compile vendor assets separately

--- gulpfile.js
+++ gulpfile.js
@@ -13,15 +13,27 @@ var data = {
     dest: 'build/stylesheets'
   },
   js: {
-    src: ['src/javascripts/**/*'].concat(mainBowerFiles('**/*.js')),
+    src: 'src/javascripts/**/*',
     dest: 'build/javascripts'
+  },
+  vendor: {
+    css: {
+      src: mainBowerFiles('**/*.css'),
+      dest: 'build/stylesheets'
+    },
+    js: {
+      src: mainBowerFiles('**/*.js'),
+      dest: 'build/javascripts'
+    }
   }
 }
 
-gulp.task('default', ['html', 'scss', 'js'], function () {
+gulp.task('default', ['html', 'scss', 'js', 'vendor-css', 'vendor-js'], function () {
   gulp.watch(data.html.src, ['html'])
   gulp.watch(data.scss.src, ['scss'])
   gulp.watch(data.js.src, ['js'])
+  gulp.watch(data.vendor.css.src, ['vendor-css'])
+  gulp.watch(data.vendor.js.src, ['vendor-js'])
 })
 
 gulp.task('html', function () {
@@ -41,4 +53,14 @@ gulp.task('js', function () {
     presets: ['es2015']
   }))
   .pipe(gulp.dest(data.js.dest))
+})
+
+gulp.task('vendor-css', function () {
+  gulp.src(data.vendor.css.src)
+  .pipe(gulp.dest(data.vendor.css.dest))
+})
+
+gulp.task('vendor-js', function () {
+  gulp.src(data.vendor.js.src)
+  .pipe(gulp.dest(data.vendor.js.dest))
 })
